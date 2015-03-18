5782db99d13d50a47de1773149a451420a34c3bd
王 云帆 <wangyunfan.xp@gmail.com>
2015-03-18 14:34:51 +0800

    DB add

---
+++ ToDoItem.h
@@ -0,0 +1,19 @@
+//
+//  ToDoItem.h
+//  ToDoList
+//
+//  Created by 王 云帆 on 15/3/18.
+//  Copyright (c) 2015年 王 云帆. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import <CoreData/CoreData.h>
+
+
+@interface ToDoItem : NSManagedObject
+
+@property (nonatomic, retain) NSString * itemName;
+@property (nonatomic, retain) NSNumber * completed;
+@property (nonatomic, retain) NSDate * creationDate;
+
+@end
--- ToDoItem.m
+++ ToDoItem.m
@@ -2,12 +2,17 @@
 //  ToDoItem.m
 //  ToDoList
 //
-//  Created by 王 云帆 on 15/3/16.
+//  Created by 王 云帆 on 15/3/18.
 //  Copyright (c) 2015年 王 云帆. All rights reserved.
 //
 
 #import "ToDoItem.h"
 
+
 @implementation ToDoItem
+
+@dynamic itemName;
+@dynamic completed;
+@dynamic creationDate;
 
 @end
